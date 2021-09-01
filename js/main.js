416782dea97fcc4c4884885cc988d5417bbe9799
daiki <88297987+dai-blog@users.noreply.github.com>
2021-09-02 08:55:37 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,33 @@
 'use strict';
 
 {
+// お問い合わせフォームお知らせ
   const submit = document.getElementById('submit');
 
   submit.addEventListener('click', (e) => {
     e.preventDefault();
     confirm('フォーム機能を実装していないので送信できません。Byだいき');
   });
+
+// メニューバー
+  const menu = document.getElementById('bars');
+  const close = document.getElementById('close');
+  const modal = document.getElementById('modal');
+
+  menu.addEventListener('click', () => {
+    modal.classList.add('appear');
+    menu.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    modal.classList.remove('appear');
+    menu.classList.remove('hide');
+  });
+
+  modal.addEventListener('click', () => {
+    modal.classList.remove('appear');
+    menu.classList.remove('hide');
+  });
+
+  
 }
